04ede3b77098bf627cf4489408a508eeaeac72be
nihaoflagma <114833329+nihaoflagma@users.noreply.github.com>
2025-01-20 23:19:16 +0300

    Update GeneratePresetImpl.java

--- heroesalgo/src/com/heroes_task/programs/GeneratePresetImpl.java
+++ heroesalgo/src/com/heroes_task/programs/GeneratePresetImpl.java
@@ -13,10 +13,7 @@ import java.util.Set;
 
 public class GeneratePresetImpl implements GeneratePreset {
 
-    // Сложность:
-    // Сортировка - O(n log n)
-    // Выбор юнитов - O(n)
-    // В итоге ~ O(n log n)
+    
     @Override
     public Army generate(List<Unit> unitList, int maxPoints) {
         Army computerArmy = new Army();
@@ -38,17 +35,17 @@ public class GeneratePresetImpl implements GeneratePreset {
         return computerArmy;
     }
 
-    // Сортировка юнитов по их эффективности
+    
     private void sortUnitsByEffectiveness(List<Unit> units) {
         units.sort(Comparator.comparingDouble(unit -> -((double) (unit.getBaseAttack() + unit.getHealth()) / unit.getCost())));
     }
 
-    // Вычисляем максимальное количество юнитов, которые можно добавить
+    
     private int calculateMaxUnitsToAdd(Unit unit, int maxPoints, int currentPoints) {
         return Math.min(11, (maxPoints - currentPoints) / unit.getCost());
     }
 
-    // Добавляем юниты в армию
+    
     private void addUnitsToArmy(Unit unit, int unitsToAdd, List<Unit> selectedUnits) {
         for (int i = 0; i < unitsToAdd; i++) {
             Unit newUnit = createNewUnit(unit, i);
@@ -56,7 +53,7 @@ public class GeneratePresetImpl implements GeneratePreset {
         }
     }
 
-    // Новый юнит с уникальным именем
+   
     private Unit createNewUnit(Unit unit, int index) {
         Unit newUnit = new Unit(unit.getName(), unit.getUnitType(), unit.getHealth(),
             unit.getBaseAttack(), unit.getCost(), unit.getAttackType(),
@@ -65,7 +62,7 @@ public class GeneratePresetImpl implements GeneratePreset {
         return newUnit;
     }
 
-    // Случайные координаты юнитам
+    
     private void assignCoordinates(List<Unit> units) {
         Set<String> occupiedCoords = new HashSet<>();
         Random random = new Random();
@@ -75,7 +72,7 @@ public class GeneratePresetImpl implements GeneratePreset {
         }
     }
 
-    // Присвоение юниту случайных координат и проверка на занятость
+    
     private void assignRandomCoordinates(Unit unit, Set<String> occupiedCoords, Random random) {
         int coordX, coordY;
         do {
